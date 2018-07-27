8a2720327173a40bde0f9266fd5b7049b39504b1
Josh <joshcho@Joshs-MacBook-Pro.local>
2018-07-27 15:28:13 -0700

    Seed database with 100 fake data, consists of usernames, images, dates, reviews

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
-const fs = require('fs');
 const faker = require('faker');
+// const dataGenerator = require('./../dataGenerator');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -18,60 +18,30 @@ connection.connect((err) => {
 });
 
 const seedData = (review) => {
-  const query = [review.userName, review.img, review.addedAt, review.review];
-  connection.query(
-    'INSERT INTO reviews (userName, img, addedAt, review) VALUES (?,?,?,?)',
-    query,
-    (err, data) => {
-      if (err) {
-        console.log('Error adding reviews to the database', err);
-        return;
-      }
-      console.log('Succesfully added to the database');
-    },
-  );
-};
-
-function csv(arr) {
-  const keys = Object.keys(arr[0]);
-  let result = `${keys.join('\t')}\n`;
-
-  arr.forEach((obj) => {
-    keys.forEach((k, ix) => {
-      if (ix) result += '\t';
-      result += obj[k];
-    });
-    result += '\n';
-  });
-
-  fs.writeFile('fakeData.js', result, (err) => {
-    if (err) {
-      console.log('Error writing data', err);
-    } else {
-      console.log('Saved!');
-    }
-  });
-}
-
-const generateFakeData = () => {
-  const arr = [];
-  for (let i = 0; i <= 10; i += 1) {
-    const review = {
+  for (let i = 1; i <= 100; i += 1) {
+    review = {
       userName: faker.name.findName(),
       img: faker.image.avatar(),
       addedAt: faker.date.recent(),
       review: faker.lorem.paragraph(),
     };
-    arr.push(review);
-    console.log(arr);
+    const query = [review.userName, review.img, review.addedAt, review.review];
+    connection.query(
+      'INSERT INTO reviews (userName, img, addedAt, review) VALUES (?,?,?,?)',
+      query,
+      (err, data) => {
+        if (err) {
+          console.log('Error adding reviews to the database', err);
+          return;
+        }
+        console.log('Succesfully added to the database', data);
+      },
+    );
   }
-  csv(arr);
 };
-
-generateFakeData();
+seedData();
 
 module.exports = {
   connection,
-  generateFakeData,
-  csv,
+  seedData,
 };
